6c11321020561da05199139d81af4c223938fa12
daboom <stefano.lazzaro.ep@gmail.com>
2014-07-23 17:37:05 +0200

    creata pagina e non popup e sistemato upload, ora funziona

--- js/upload.js
+++ js/upload.js
@@ -1,53 +1,37 @@
+//at click on browser action it opens up a new tab
+chrome.browserAction.onClicked.addListener(function(activeTab){
+	var newURL = "pages/upload.html";
+	chrome.tabs.create({ url: newURL });
+});
+
+////vars
 var data = new FormData(); //used to store data to upload in Alfresco
 var resp; //stores ajax response
 var SUCCESS = 0;
 var FAILURE = 1;
 
-/////drag and drop file handler///////////////
-var obj = $("#file-upload-wrapper");
-obj.on('dragenter', function (e) {
+////listeners
+//document.addEventListener("DOMContentLoaded", restoreOptions);
+//document.getElementById("send").addEventListener("click", sendToAlfresco);
+//document.getElementById("files").addEventListener("change", storeFiles);
+$("#filedata").on("dragenter", function (e) {
 	e.stopPropagation();
 	e.preventDefault();
-	$(this).css('border', '3px solid #0B85A1');
+	$(this).css("border", "3px solid #0B85A1");
 });
-obj.on('dragover', function (e) {
-	e.stopPropagation();
+$("#filedata").change(function (e) {
+	$("#filedata").css("border", "3px dotted #0B85A1");
 	e.preventDefault();
-});
-obj.on('drop', function (e) {
-	$(this).css('border', '3px dotted #0B85A1');
-	storeFiles(e);
-});
-////////////////////////////////
-
-//store files on local variable
-function storeFiles(event) {
-	event.preventDefault();
-	
-	//store files data
 	var files = event.target.files || event.originalEvent.dataTransfer.files;
-	
-	for (var i = 0, f; f = files[i]; i++) {
-		console.log("f[" + i + "].name = " + f.name);
-		console.log("f[" + i + "].size = " + f.size);
-		console.log("f[" + i + "].lastModifiedDate = " + f.lastModifiedDate.toLocaleDateString());
-		
-		//data.append("file", f);
-		data = f;
-	}
-
-	//SOLO PER LOG DEBUG
-	for (var i = 0, d; d = data[i]; i++) {
-		console.log("d[" + i + "] = " + d);
-	}
-	
 	showMessage("stored " + files.length + " files", SUCCESS);
-}
+});
+$("#submit").click(upload); //submit button upload to Alfresco
+/////////////
 
 //mostra info su file caricati
-function fileInfo(event) {
-	event.preventDefault();
-	
+function showFileInfo(event) {
+	$("#filedata").css("border", "3px dotted #0B85A1");
+	event.preventDefault();	
 	//store files data
 	var files = event.target.files || event.originalEvent.dataTransfer.files;
 	
@@ -59,26 +43,18 @@ function fileInfo(event) {
 	showMessage("stored " + files.length + " files", SUCCESS);
 }
 
-//upload file to Alfresco Site
-function sendToAlfresco() {
-	var alfrescoRoot = "http://intra.e-projectsrl.net/alfresco";
+/**
+* Carico il file sulla repo Alfresco 
+*/
+function upload() {
+	var alfrescoRoot = "http://intra.e-projectsrl.net/alfresco"; //ep alfresco
 	var usr = {
-		"username": document.getElementById('username').value, 
-		"password": document.getElementById('password').value
+		"username": $("#username").val(), 
+		"password": $("#password").val()
 	};
-	var fileDataForm = {
-		"filedata": data, //aggiungo l'oggetto contenente i file
-		"siteid": "repository",
-		"containerid": "documentLibrary",
-		"uploaddirectory": "Rapportini",
-		"description": "nuova versione",
-		"contenttype": "cm:content",
-		"filename": "puntamenti_utili.txt",
-		"overwrite": "true"
-	};
-	console.log("data = " + data);
-	console.log("JSON.stringify(data) = " + JSON.stringify(data));
-	console.log("JSON.stringify(fileDataForm) = " + JSON.stringify(fileDataForm));
+	
+	//preparo i dati per l'upload prendendoli dal form in pagina
+	var formData = new FormData(document.getElementById("upload-form"));
 	
 	//koopa: DEBUG-MODE ONLY
 	if (document.getElementById('debug').checked) {
@@ -94,10 +70,9 @@ function sendToAlfresco() {
 		contentType: "application/json; charset=utf-8", //questo è fondamentale
 		data: JSON.stringify(usr),
 		success: function (json) {
-			console.log("resp = " + JSON.stringify(json));
+			console.log("login-resp = " + JSON.stringify(json));
 			resp = JSON.parse(JSON.stringify(json));
 			showMessage("LOGIN OK!", SUCCESS);
-
 			var ticket = resp.data.ticket; //salvo il ticket per effettuare il caricamento
 			
 			//carico file su Alfresco col ticket appena ricevuto
@@ -105,46 +80,42 @@ function sendToAlfresco() {
 				type: "POST",
 				url: alfrescoRoot + "/service/api/upload?alf_ticket=" + ticket,
 				cache: false,
-				//contentType: "multipart/form-data",
 				contentType: false, //altrimenti jQuery manipola i dati
 				processData: false,
 				dataType: "json",
-				data: JSON.stringify(fileDataForm),
+				//data: JSON.stringify(formData),
+				data: formData,
 				success: function (json) {     
-					console.log("resp = " + JSON.parse( JSON.stringify(json)) );
+					console.log("upload-resp = " + JSON.stringify(json) );
 					console.log("UPLOAD OK");
 					showMessage("UPLOAD OK", SUCCESS);
 				},
 				error: function (json) {
-					console.log("resp = " + JSON.stringify(json) );
-					console.log("UPLOAD KO");
-					showMessage("UPLOAD KO", FAILURE);
+					//console.log("upload-resp = " + JSON.stringify(json));
+					manageAjaxError(json);
 				}
 			});
 		},
 		error: function (json) {
-			console.log("resp = " + JSON.stringify(json));
-			resp = JSON.parse(JSON.stringify(json));
-			var message;
-			if (resp.responseJSON !== undefined ) {
-				message = "code: " + resp.responseJSON.status.code + 
-					"\nname: " + resp.responseJSON.status.name + "\ndescription: " + 
-					resp.responseJSON.status.description;
-			}
-			else {
-				message = "Unknown Error (maybe " + alfrescoRoot + " is not available";
-			}
-			console.log(message);
-			showMessage(message, FAILURE);
+			console.log("login-resp = " + JSON.stringify(json));
+			manageAjaxError(json);
 		}
 	});
-	
 }
 
-function upload() {
-	console.log("upload new!");
-	var form = document.getElementById("upload-form");
-	var formData = new FormData(form);
+function manageAjaxError(json) {
+	console.log("upload-resp = " + JSON.stringify(json) );
+	resp = JSON.parse(JSON.stringify(json));
+	var message;
+	if (resp.responseJSON !== undefined ) {
+		//message = "code: " + resp.responseJSON.status.code + "\nname: " + resp.responseJSON.status.name + "\ndescription: " + resp.responseJSON.status.description + "\nmessage: " + resp.responseJSON.message;
+		message = resp.responseJSON.message;;
+	}
+	else {
+		message = "Unknown Error (maybe " + alfrescoRoot + " is not available";
+	}
+	console.log(message);
+	showMessage(message, FAILURE);
 }
 
 //show message in page (type=0 means success, type=1 means error)
@@ -197,10 +168,3 @@ function alfrescoAjax() {
 		execScripts: true // Automatically executes script tags (<script>) returned in the forms engine's response
 	});
 }
-
-//add listener
-//document.addEventListener("DOMContentLoaded", restoreOptions);
-//document.getElementById("send").addEventListener("click", sendToAlfresco);
-//document.getElementById("files").addEventListener("change", storeFiles);
-document.getElementById("filedata").addEventListener("change", fileInfo);
-document.getElementById("submit").addEventListener("click", upload); //submit button upload to Alfresco
