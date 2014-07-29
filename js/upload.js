8f7f05bb5a5af127a71a489d786b87d26cd6c672
daboom <stefano.lazzaro.ep@gmail.com>
2014-07-29 17:19:31 +0200

    aggiunta progress-bar figa e sistemato il form

--- js/upload.js
+++ js/upload.js
@@ -7,31 +7,28 @@ chrome.browserAction.onClicked.addListener(function(activeTab){
 });
 */
 
-//apre pagina di popup
-chrome.browserAction.onClicked.addListener(function() {
-	var w = 500;
-	var h = 500;
-	var left = (screen.width/2) - (w/2);
-	var top = (screen.height/2) - ( h/2);
-	chrome.windows.create({'url': 'pages/upload.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
-		initParams();
-	});
-});
-
 ////vars
 var data = new FormData(); //used to store data to upload in Alfresco
 var resp; //stores ajax response
 var SUCCESS = 0;
 var FAILURE = 1;
-var PERCENT_10 = 0.1; //login percentage
-var PERCENT_90 = 0.9; //alfresco file upload
+var PERCENT_15 = 0.15; //login percentage
+var PERCENT_75 = 0.75; //alfresco file upload
 var alfrescoRoot = "http://intra.e-projectsrl.net/alfresco"; //ep alfresco
-var bar = $("#bar");
-//var percent = $(".percent");
-var status = $("#status-message");
+alfrescoRoot = "http://localhost:8080/alfresco";
+initParams(); //inizializzo parametri
+
 
 ////listeners
 //document.addEventListener("DOMContentLoaded", restoreOptions);
+chrome.browserAction.onClicked.addListener(function() { //apre pagina di popup
+	var w = 500;
+	var h = 580;
+	var left = (screen.width/2) - (w/2);
+	var top = (screen.height/2) - ( h/2);
+	chrome.windows.create({'url': 'pages/upload.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
+	});
+});
 $("#filedata").on("dragenter", function (e) {
 	e.stopPropagation();
 	e.preventDefault();
@@ -49,14 +46,8 @@ $("#overwrite").change(function (e) {
 });
 /////////////
 
-//DEBUG-MODE ONLY
-if (document.getElementById('debug').checked) {
-	console.log("DEBUG-MODE: ON");
-	alfrescoRoot = "http://localhost:8080/alfresco";
-}
-////////
-
 function initParams() {
+	console.log("Inizializzo valore di overwrite");
 	$("#overwrite").val($("#overwrite").is(":checked"));
 }
 
@@ -76,10 +67,12 @@ function showFileInfo(event) {
 }
 
 function checkFormParam() {
+	var report = "";
 	$("input").each(function(input) {
 		console.log($( this ).attr("id") + ": " + $( this ).val() );
-		$("#status-message").append($( this ).attr("id") + ": " + $( this ).val() + "<br>");
+		report += ($( this ).attr("id") + ": " + $( this ).val() + "<br>");
 	});
+	$("status-message").html(report);
 }
 
 /**
@@ -87,10 +80,10 @@ function checkFormParam() {
 */
 function upload() {
 	//stoppo animazioni se ce ne sono attive
-	$("#status").finish();
+	//$("#status").finish();
 	//pulisco area messaggi
 	$("#status-message").empty();
-	
+		
 	//preparo i dati per l'upload prendendoli dal form in pagina
 	var usr = {
 		"username": $("#username").val(), 
@@ -106,23 +99,23 @@ function upload() {
 		
 		//prima
 		beforeSend: function() {
-			$("#status").show();
+			//$("#status-bar").show();
 			showMessage("Login...", SUCCESS);
+			NProgress.start();
 		},
-
 		success: function (json) {
+			NProgress.inc(PERCENT_15); //after login
 			console.log("login-resp = " + JSON.stringify(json));
 			resp = JSON.parse(JSON.stringify(json));
-			$("#bar").val(PERCENT_10);
 			showMessage("LOGIN OK!", SUCCESS);
 			var ticket = resp.data.ticket; //salvo il ticket per effettuare il caricamento
 			
 			//ajax per upload del form con il file
 			alfrescoUpload(ticket);
-		},
-		
+		},	
 		error: function (json) {
 			console.log("login-resp = " + JSON.stringify(json));
+			NProgress.done();
 			manageAjaxError(json);
 		}
 	});
@@ -145,37 +138,31 @@ function alfrescoUpload(ticket) {
 			console.log("Upload...");
 		},				
 				
-		//aggiornamento barra durante l'upload dei dati
+		//aggiornamento progress-bar durante l'upload dei dati
 		xhr: function() {
 			var xhr = new window.XMLHttpRequest();
 			xhr.upload.addEventListener("progress", function(e) {
 				if (e.lengthComputable) {
 					var percentComplete = e.loaded / e.total;
-					percentComplete = PERCENT_10 + percentComplete * PERCENT_90;
-					$("#bar").val(percentComplete);
-					console.log("percent = " + percentComplete);
-					if (percentComplete === 1) {
-						//percentComplete
-					}
+					percentComplete = PERCENT_15 + percentComplete * PERCENT_75;
+					NProgress.set(percentComplete);
 				}
 			}, false);
 			return xhr;
-		},				
-				
-		success: function (json) {     
+		},						
+		success: function (json) {   
+			console.log("upload-success");
+			showMessage("UPLOAD OK", SUCCESS);		
 			console.log("upload-resp = " + JSON.stringify(json) );
 			console.log("UPLOAD OK");
-			showMessage("UPLOAD OK", SUCCESS);
 		},
-		
 		error: function (json) {
 			//console.log("upload-resp = " + JSON.stringify(json));
 			manageAjaxError(json);
 		},
-		
 		complete: function () {
-			//faccio sparire la barra di caricamento
-			$("#status").delay(4000).fadeOut("slow");
+			console.log("upload-complete");
+			NProgress.done();
 		}
 	});
 }
@@ -189,7 +176,7 @@ function manageAjaxError(json) {
 		message = resp.responseJSON.message;;
 	}
 	else {
-		message = "Unknown Error (maybe " + alfrescoRoot + " is not available";
+		message = "Unknown Error (maybe " + alfrescoRoot + " is not available)";
 	}
 	console.log(message);
 	showMessage(message, FAILURE);
@@ -201,8 +188,9 @@ function showMessage(message, type) {
 		$("#status-message").css("color", "green");
 	else
 		$("#status-message").css("color", "red");
-	//$("#status-message").html(message);
+	$("#status-message").empty();
 	$("#status-message").append(message);
+	
 }
 
 //restores input box state using the preferences stored in chrome.storage.
@@ -218,29 +206,5 @@ function restoreOptions() {
 		$("#folder").value = items.folder;
 		$("#file").value = items.file;
 		$("#overwrite").value = items.overwrite;
-	});
-}
-
-/** Uses alfresco.js */
-function alfrescoAjax() {
-	// Perform AJAX request to get form content from Forms Engine
-	Alfresco.util.Ajax.request({
-		url: Alfresco.constants.URL_SERVICECONTEXT + "components/form",
-		dataObj:
-		{
-			htmlid: this.options.htmlid, // Ensure unique IDs
-			itemKind : "node", // It is fixed at the moment but could be dynamic in the future
-			itemId : this.options.itemId,
-			mode: this.options.mode,
-			submitType: "json",
-			formUI: true,
-			showCancelButton: true
-		},
-		successCallback : {
-
-		},
-		failureMessage : Alfresco.util.message("message.failure"), // Generic failure message
-		scope: this,
-		execScripts: true // Automatically executes script tags (<script>) returned in the forms engine's response
 	});
 }
